a3e785881b13279cb9d7b98b7df5a2a03bd831db
Luís Nascimento de Sousa <luisrocksousa1111@gmail.com>
2024-03-08 20:30:42 -0300

    api-node e express

---
+++ aula04/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(function(req, res){
+    console.log("Data", new Date());
+});
+
+app.get("/", function(req, res){
+    console.log("URL=", req.url);
+    console.log("Método=", req.method);
+    console.log("Parâmetros de consulta=", req.query);
+    console.log("Cabeçalhos=", req.headers);
+    res.send("Olá");
+});
+
+app.post("/", function(req, res) {
+    res.json({ status: "200", message: "Sucesso" });
+});
+
+app.put("/", function(req, res){
+    res.status(204).end();
+});
+
+    app.delete("/", function(req, res){
+        throw new Exception();
+    })
+
+    app.use(function(error, req, res, next){
+        res.status(400).send("Deu ruim!");
+    });
+
+app.listen(3000, function() {
+    console.log("API está on!");
+});
