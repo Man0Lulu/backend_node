e11cd19b432ec510d4e5f7abdef1f061a2893620
Luís Nascimento de Sousa <luisrocksousa1111@gmail.com>
2024-03-12 22:42:33 -0300

    Finalizado a API

--- aula05/router.js
+++ aula05/router.js
@@ -2,17 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
+const produtos = [{id: 1, nome: "Uva", preco: 10.00}];
+
 router.get("/produtos", function(req, res){
-  res.json([]);
+  res.json(produtos);
 });
 
 router.get("/produtos/:produtoId", function(req, res){
-  if (req.params.produtoId == 100000) {
+  const encontrado = produtos.find((produto) => 
+    produto.id == req.params.produtoId);
+  
+  if (!encontrado) {
     res.status(404).json({msg: "Produto não encontrado"});
     return;
   }
 
-  res.json({});
+  res.json(encontrado);
 });
 
 router.post("/produtos", function(req, res){
@@ -20,24 +25,37 @@ router.post("/produtos", function(req, res){
     res.status(422).json({msg: "Nome e/ou preco do produto obrigatorios"});
     return;
   }
-  res.status(201).json({});
+  const novo = {id: produtos.length+1, 
+    nome: req.body.nome, preco: req.body.preco};
+  produtos.push(novo);
+  res.status(201).json(novo);
 });
 
 router.put("/produtos/:produtoId", function(req, res){
-  if (req.params.produtoId == 100000) {
+ const encontrado = produtos.find((produto) => 
+    produto.id == req.params.produtoId);
+  
+  if (!encontrado) {
     res.status(404).json({msg: "Produto não encontrado"});
     return;
   }
+
+  encontrado.nome = req.body.nome;
+  encontrado.preco = req.body.preco;
   
-  res.json({});
+  res.json(encontrado);
 });
 
 router.delete("/produtos/:produtoId", function(req, res){
-  if (req.params.produtoId == 100000) {
+  const posicao = produtos.findIndex((produto) =>
+    produto.id == req.params.produtoId);
+
+  if (posicao < 0) {
     res.status(404).json({msg: "Produto não encontrado"});
     return;
   }
     
+  produtos.splice(posicao, 1);
   res.status(204).end();
 });
   
