ea7578aebba077b2a393e9305ef0347304f973db
Luís Nascimento de Sousa <luisrocksousa1111@gmail.com>
2024-04-10 00:16:33 -0300

    colocando a ultima parte das rotas

--- pratica06/app.js
+++ pratica06/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
-app.use('/produtos', produtosRouter)
+app.use('/produtos', produtosRouter) 
 //app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 
