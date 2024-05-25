ec97cc82faf7c3e9a468ed4beef7d440a49b8f14
Luís Nascimento de Sousa <luisrocksousa1111@gmail.com>
2024-05-24 21:51:51 -0300

    criando o token jwt completo

--- aula11/controllers/users.js
+++ aula11/controllers/users.js
@@ -1,21 +1,44 @@
-const Usuario = require('../models/users')
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const Usuario = require("../models/users");
 
-async function criar(req, res){
-    const novoUsuario = await Usuario.create(req.body)
-    res.status(201).json({ id: novoUsuario._id.toString(), email: novoUsuario.email })
+function cifrarSenha(senha, salto) {
+   const hash = crypto.createHmac("sha256", salto);
+   hash.update(senha);
+   return hash.digest('hex');
 }
 
-async function entrar(req, res){
-    const usuarioEncontrado = await Usuario.findOne({ email: req.body.email });
-    if (usuarioEncontrado){
-        if(usuarioEncontrado.senha === req.body.senha) {
-            res.json({token: '1234567'})
-        }else{
-            res.status(401).json({ msg: 'acesso negado '})
-        }
-    }else{
-        res.status(400).json({ msg: 'credenciais invalidas'})
+async function criar(req, res) {
+  const salto = crypto.randomBytes(16).toString('hex');
+  const senhaCifrada = cifrarSenha(req.body.senha, salto);
+
+  const novoUsuario = await Usuario.create({
+    email: req.body.email,
+    senha: senhaCifrada,
+    salto
+   });
+  res
+    .status(201)
+    .json({ 
+      id: novoUsuario._id.toString(), 
+      email: novoUsuario.email,
+      senha: novoUsuario.senha,
+      salto: novoUsuario.salto
+     });
+}
+
+async function entrar(req, res) {
+  const usuarioEncontrado = await Usuario.findOne({ email: req.body.email });
+  if (usuarioEncontrado) {
+    const senhaCifrada = cifrarSenha(req.body.senha, usuarioEncontrado.salto);
+    if (usuarioEncontrado.senha === senhaCifrada) {
+      res.json({ token: jwt.sign({email: usuarioEncontrado.email}, process.env.SEGREDO, {expiresIn: '5m'}) });
+    } else {
+      res.status(401).json({ msg: "acesso negado" });
     }
+  } else {
+    res.status(400).json({ msg: "credenciais invalidas" });
+  }
 }
 
-module.exports = {criar, entrar}
+module.exports = { criar, entrar };
--- aula11/models/users.js
+++ aula11/models/users.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
 email: String,
 senha: String,
+salto: String
 });
 
 module.exports = mongoose.model("Usuario", userSchema);
