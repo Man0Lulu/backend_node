8f66d191a6bc62f0a5950c0c62169a629a5805de
Luís Nascimento de Sousa <luisrocksousa1111@gmail.com>
2024-05-28 23:11:17 -0300

    testes e outras coisas

--- pratica09/app.js
+++ pratica09/app.js
@@ -12,5 +12,5 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use('/api-docs', Apidocs)
 module.exports = app;
--- pratica09/tests/produtos.test.js
+++ pratica09/tests/produtos.test.js
@@ -1,6 +1,8 @@
-const request = require('supertest');
+const supertest = require('supertest');
 const app = require('./app');
+const request = supertest(app)
 
+let id = null
 describe('GET /api-docs', () => {
     it('deve retornar um objeto JSON 200', async () => {
       const response = await request(app).get('/api-docs');
@@ -8,3 +10,12 @@ describe('GET /api-docs', () => {
       expect(response.text).toBe('Busca de usuario');
     });
   });
+describe('POST /produtos', () => {
+  it('deve reotrnar um objeto JSON 201', async () => {
+    const response = await request.post('/produtos')
+    .send({"nome": "uva", "preco": 20.0})
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('nome','uva')
+    id = response.body._id.toString
+  })
+})
